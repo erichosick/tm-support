a5a984ddf057ed366d7e12819c964b0ba98e3e09
Eric Hosick <erichosick@gmail.com>
2022-10-19 14:07:00 -0700

    Removed unused imports

--- packages/object-json-path/__tests__/index.integration.spec.ts
+++ packages/object-json-path/__tests__/index.integration.spec.ts
@@ -1,5 +1,3 @@
-import { insertIntoObject, getFromObject } from '../src/index';
-
 describe('should only run during integration testing', () => {
   it('should only run during an integration test', () => {
     expect(1).toEqual(1);
